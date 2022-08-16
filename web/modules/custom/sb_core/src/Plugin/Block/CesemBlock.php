ad2106ddefc6e84811faba9289c6b9e0678fcab9
Alexandre Dias <alex.jm.dias@gmail.com>
2022-08-16 14:22:03 +0100

    Add new theme and cesem blocks.

---
+++ web/modules/custom/sb_core/src/Plugin/Block/CesemBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\sb_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Cesem' block.
+ *
+ * @Block(
+ *  id = "cesem_block",
+ *  admin_label = @Translation("Cesem block"),
+ * )
+ */
+class CesemBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    return [
+      '#markup' => '<p class="f7 lh-copy tc gray-500">' . $this->t('© @year CESEM – Centro de Estudos de Sociologia e Estética Musical', ['@year' => date("Y")]) . '</p>',
+    ];
+  }
+
+}
--- web/modules/custom/sb_core/src/Utility/MaterialityTypesHelper.php
+++ web/modules/custom/sb_core/src/Utility/MaterialityTypesHelper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\sb_core\Utility;
 
 /**
